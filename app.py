69e0081fcd82cb9a85538c000301823132718202
Hagliberto <hagliberto@gmail.com>
2024-05-05 01:47:50 -0300

    Controle de quantidade de páginas

--- app.py
+++ app.py
@@ -148,8 +148,8 @@ def main():
             if st.sidebar.button("Voltar para a primeira página", key="first_page_personagem"):
                 page_number = 1
         
-    # Atualizar dados com a nova página
-    st.session_state[f"{endpoint}_page"] = page_number
+        # Atualizar dados com a nova página
+        st.session_state[f"{endpoint}_page"] = page_number
     
     # Informar em qual página se encontra
     st.sidebar.success(f"Você está na página {page_number}/{num_pages}")
